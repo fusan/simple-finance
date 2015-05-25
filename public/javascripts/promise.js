b7113b99ce9b1536c6183f5e3dbd9d3e7a3e451b
fusan <gogyaru@gmail.com>
2015-05-25 19:56:53 +0900

    更新条件変更

--- public/javascripts/promise.js
+++ public/javascripts/promise.js
@@ -54,7 +54,7 @@ $(function(){
 	var now = new Date().getHours();					//現在時刻 -> テストする際はinitialTimeの７日後を指定する。
 	console.log(now - initialTime,now, initialTime);
 
-	if (true)　{//テストの際は　条件式にtrueに (now - initialTime) == 0
+	if ((now - initialTime) == 0)　{//テストの際は　条件式にtrueに (now - initialTime) == 0
 		//通知
 		console.log('更新作業');
 		$('#input').parent().append('<span id="update">更新開始</span>')
